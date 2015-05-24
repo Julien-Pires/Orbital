c1adbe07cad355c8a7991b1be048465cb3005f52
Julien Pires <jpires@ohbibi.com>
2015-05-24 23:58:09 +0200

    Add item line

---
+++ Editor/GUI/ItemLine.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+using Orbital.Data;
+
+namespace Orbital.GUI
+{
+    internal sealed class ItemLine
+    {
+        #region Fields
+
+        private List<ItemLine> _children;
+
+        #endregion
+
+        #region Properties
+
+        public IValueSource DataSource { get; internal set; }
+
+        public IVisual Visual { get; internal set; }
+
+        #endregion
+    }
+}
